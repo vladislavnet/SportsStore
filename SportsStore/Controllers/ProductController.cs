a9120a3ce894e23673357ace3f619ad43471f1b3
vladislavnet <vladislaus.panyukov@yandex.ru>
2020-10-18 11:27:11 +0300

    Added product pagination to the List action method in the Product controller

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -1,16 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ViewEngines;
 using SportsStore.Models;
+using System.Linq;
 
 namespace SportsStore.Controllers
 {
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        private int PageSize = 4;
         public ProductController(IProductRepository repository)
         {
             this.repository = repository;
         }
-        public ViewResult List() => View(repository.Products);
+        public ViewResult List(int productPage = 1) 
+            => View(repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((productPage - 1) * PageSize)
+                .Take(PageSize));
+        
     }
 }
