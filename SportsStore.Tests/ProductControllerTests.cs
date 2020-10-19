5ad14c166ba8247cc25611c208244bbecdbcb302
vladislavnet <vladislaus.panyukov@yandex.ru>
2020-10-19 17:24:27 +0300

    Added tests to check filtering on categories

--- SportsStore.Tests/ProductControllerTests.cs
+++ SportsStore.Tests/ProductControllerTests.cs
@@ -60,5 +60,30 @@ namespace SportsStore.Tests
             Assert.Equal(5, pageInfo.TotalItems);
             Assert.Equal(2, pageInfo.TotalPages);
         }
+
+
+        [Fact]
+        public void CanFilterProducts()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product { ProductID = 1, Name = "P1", Category = "Cat1" },
+                new Product { ProductID = 2, Name = "P2", Category = "Cat2" },
+                new Product { ProductID = 3, Name = "P3", Category = "Cat1" },
+                new Product { ProductID = 4, Name = "P4", Category = "Cat2" },
+                new Product { ProductID = 5, Name = "P5", Category = "Cat3" },
+            }).AsQueryable());
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            ProductListViewModel results = controller.List("Cat2", 1)
+                .ViewData.Model as ProductListViewModel;
+
+            Product[] prodArray = results.Products.ToArray();
+            Assert.True(prodArray.Length == 2);
+            Assert.True(prodArray[0].Name == "P2" && prodArray[0].Category == "Cat2");
+            Assert.True(prodArray[1].Name == "P4" && prodArray[1].Category == "Cat2");
+        }
     }
 }
