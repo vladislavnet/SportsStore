9db258a8ef7058115acec945988b6f7beede01c0
vladislavnet <vladislaus.panyukov@yandex.ru>
2020-10-23 11:02:13 +0300

    Creating a controller to manage the product catalog

---
+++ SportsStore/Controllers/AdminContoller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class AdminContoller : Controller
+    {
+        private IProductRepository repository;
+
+        public AdminContoller(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index() => View(repository.Products);
+    }
+}
