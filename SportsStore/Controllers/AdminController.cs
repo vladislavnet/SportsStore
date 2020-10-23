111d341f3a6dc63fc5993b0a26ae0ffdf505df09
vladislavnet <vladislaus.panyukov@yandex.ru>
2020-10-23 14:42:21 +0300

    implemented Delete method in AdminController

--- SportsStore/Controllers/AdminController.cs
+++ SportsStore/Controllers/AdminController.cs
@@ -34,5 +34,16 @@ namespace SportsStore.Controllers
         }
 
         public ViewResult Create() => View("Edit", new Product());
+
+        [HttpPost]
+        public IActionResult Delete(int productId)
+        {
+            Product deleteProduct = repository.DeleteProduct(productId);
+            if(deleteProduct != null)
+            {
+                TempData["message"] = $"{deleteProduct.Name} was deleted";
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
