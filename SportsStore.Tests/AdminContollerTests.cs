b70572c020c95b52e8903d6c4fd5cda3d8c64abb
vladislavnet <vladislaus.panyukov@yandex.ru>
2020-10-23 11:39:56 +0300

    Implemented tests for Edit method in AdminController

--- SportsStore.Tests/AdminContollerTests.cs
+++ SportsStore.Tests/AdminContollerTests.cs
@@ -22,16 +22,57 @@ namespace SportsStore.Tests
             }).AsQueryable());
             AdminController contoller = new AdminController(mock.Object);
 
-            Product[] result = GetViewMode<IEnumerable<Product>>(contoller.Index())?.ToArray();
+            Product[] result = GetViewModel<IEnumerable<Product>>(contoller.Index())?.ToArray();
 
             Assert.Equal(3, result.Length);
             Assert.Equal("P1", result[0].Name);
             Assert.Equal("P2", result[1].Name);
             Assert.Equal("P3", result[2].Name);
         }
-        private T GetViewMode<T>(IActionResult result) where T : class
+
+        [Fact]
+        public void CanEditProduct()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product { ProductID = 1, Name = "P1" },
+                new Product { ProductID = 2, Name = "P2" },
+                new Product { ProductID = 3, Name = "P3" },
+            }).AsQueryable());
+            AdminController contoller = new AdminController(mock.Object);
+
+            Product p1 = GetViewModel<Product>(contoller.Edit(1));
+            Product p2 = GetViewModel<Product>(contoller.Edit(2));
+            Product p3 = GetViewModel<Product>(contoller.Edit(3));
+
+            Assert.Equal(1, p1.ProductID);
+            Assert.Equal(2, p2.ProductID);
+            Assert.Equal(3, p3.ProductID);
+        }
+
+        [Fact]
+        public void CanEditNonexistentProduct()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product { ProductID = 1, Name = "P1" },
+                new Product { ProductID = 2, Name = "P2" },
+                new Product { ProductID = 3, Name = "P3" },
+            }).AsQueryable());
+            AdminController contoller = new AdminController(mock.Object);
+
+            Product p = GetViewModel<Product>(contoller.Edit(4));
+
+            Assert.Null(p);
+        }
+
+        private T GetViewModel<T>(IActionResult result) where T : class
         {
             return (result as ViewResult)?.ViewData.Model as T;
         }
     }
+
+    
 }
