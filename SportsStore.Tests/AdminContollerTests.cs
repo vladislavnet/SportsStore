4a8b6c89f3afc590228a63615f693415377eaea7
vladislavnet <vladislaus.panyukov@yandex.ru>
2020-10-23 11:11:10 +0300

    Created tests for AdminController

---
+++ SportsStore.Tests/AdminContollerTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SportsStore.Controllers;
+using SportsStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace SportsStore.Tests
+{
+    public class AdminContollerTests
+    {
+        [Fact]
+        public void IndexContainsAllProducts()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product { ProductID = 1, Name = "P1" },
+                new Product { ProductID = 2, Name = "P2" },
+                new Product { ProductID = 3, Name = "P3" },
+            }).AsQueryable());
+            AdminContoller contoller = new AdminContoller(mock.Object);
+
+            Product[] result = GetViewMode<IEnumerable<Product>>(contoller.Index())?.ToArray();
+
+            Assert.Equal(3, result.Length);
+            Assert.Equal("P1", result[0].Name);
+            Assert.Equal("P2", result[1].Name);
+            Assert.Equal("P3", result[2].Name);
+        }
+        private T GetViewMode<T>(IActionResult result) where T : class
+        {
+            return (result as ViewResult)?.ViewData.Model as T;
+        }
+    }
+}
