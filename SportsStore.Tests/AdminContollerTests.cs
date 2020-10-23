a52a18bf90da6a13740ef1d6377ecc6d47afa228
vladislavnet <vladislaus.panyukov@yandex.ru>
2020-10-23 12:35:47 +0300

    Implemented tests for product editing

--- SportsStore.Tests/AdminContollerTests.cs
+++ SportsStore.Tests/AdminContollerTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Moq;
 using SportsStore.Controllers;
 using SportsStore.Models;
@@ -66,6 +67,38 @@ namespace SportsStore.Tests
             Product p = GetViewModel<Product>(contoller.Edit(4));
 
             Assert.Null(p);
+        }
+
+        [Fact]
+        public void CanSaveValidChanges()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            Mock<ITempDataDictionary> tempData = new Mock<ITempDataDictionary>();
+            AdminController controller = new AdminController(mock.Object)
+            {
+                TempData = tempData.Object
+            };
+            Product product = new Product { Name = "Test" };
+
+            IActionResult result = controller.Edit(product);
+
+            mock.Verify(m => m.SaveProduct(product));
+            Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Index", (result as RedirectToActionResult).ActionName);
+        }
+
+        [Fact]
+        public void CannotSaveValidChanges()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            AdminController controller = new AdminController(mock.Object);
+            Product product = new Product { Name = "Test" };
+            controller.ModelState.AddModelError("error", "error");
+
+            IActionResult result = controller.Edit(product);
+
+            mock.Verify(m => m.SaveProduct(It.IsAny<Product>()), Times.Never);
+            Assert.IsType<ViewResult>(result);
         }
 
         private T GetViewModel<T>(IActionResult result) where T : class
